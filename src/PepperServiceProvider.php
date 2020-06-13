96282ba52f9b33c97adc8d9440d9cb948a7aa118
Amirmasoud <amirmasood33@gmail.com>
2020-06-13 23:15:13 +0430

    Init

---
+++ src/PepperServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace TerraCotta\Pepper;
+
+use Illuminate\Support\ServiceProvider;
+
+class PepperServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        $this->publishes([
+            __DIR__ . '/../config/pepper.php' => config_path('pepper.php'),
+        ], 'config');
+
+        $this->publishes([
+            __DIR__ . '/../resources/lang' => "{$this->app['path.lang']}/vendor/pepper",
+        ]);
+
+        $this->loadTranslationsFrom(__DIR__ . '/../resources/lang/', 'pepper');
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/pepper.php', 'pepper');
+    }
+}
