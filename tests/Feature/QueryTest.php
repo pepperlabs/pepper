a528f9be7d2936363e95322beb5a8b950ffacad8
Amirmasoud <amirmasood33@gmail.com>
2020-08-21 22:24:06 +0430

    WIP

--- tests/Feature/QueryTest.php
+++ tests/Feature/QueryTest.php
@@ -9,13 +9,31 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 class QueryTest extends TestCase
 {
+    public function testStatus()
+    {
+        $response = dd($this->getJson('http://localhost:8000/graphql'));
+
+        $response->assertStatus(200);
+    }
+
     /**
      * A basic test example.
      *
      * @return void
      */
-    public function testBasicTest()
+    public function testSimpleQueryByPk()
     {
-        // ...
+        $response = $this->withHeaders([
+            'Content-Type' => 'application/json',
+        ])->json('GET', '/graphql', [
+            'query' => 'query {user_by_pk(id: 1){id}}'
+        ]);
+        $response->assertStatus(200)->assertExactJson([
+            'data' => [
+                'user_by_pk' => [
+                    ['id' => '1']
+                ]
+            ]
+        ]);
     }
 }
