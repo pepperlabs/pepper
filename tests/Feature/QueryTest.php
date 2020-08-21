15ca68707abd1ff81c5b1b4b314e2844df493560
Amirmasoud <amirmasood33@gmail.com>
2020-08-21 22:24:27 +0430

    WIP

--- tests/Feature/QueryTest.php
+++ tests/Feature/QueryTest.php
@@ -9,31 +9,31 @@ use Illuminate\Foundation\Testing\WithoutMiddleware;
 
 class QueryTest extends TestCase
 {
-    public function testStatus()
-    {
-        $response = dd($this->getJson('http://localhost:8000/graphql'));
+    // public function testStatus()
+    // {
+    //     $response = dd($this->getJson('http://localhost:8000/graphql'));
 
-        $response->assertStatus(200);
-    }
+    //     $response->assertStatus(200);
+    // }
 
-    /**
-     * A basic test example.
-     *
-     * @return void
-     */
-    public function testSimpleQueryByPk()
-    {
-        $response = $this->withHeaders([
-            'Content-Type' => 'application/json',
-        ])->json('GET', '/graphql', [
-            'query' => 'query {user_by_pk(id: 1){id}}'
-        ]);
-        $response->assertStatus(200)->assertExactJson([
-            'data' => [
-                'user_by_pk' => [
-                    ['id' => '1']
-                ]
-            ]
-        ]);
-    }
+    // /**
+    //  * A basic test example.
+    //  *
+    //  * @return void
+    //  */
+    // public function testSimpleQueryByPk()
+    // {
+    //     $response = $this->withHeaders([
+    //         'Content-Type' => 'application/json',
+    //     ])->json('GET', '/graphql', [
+    //         'query' => 'query {user_by_pk(id: 1){id}}'
+    //     ]);
+    //     $response->assertStatus(200)->assertExactJson([
+    //         'data' => [
+    //             'user_by_pk' => [
+    //                 ['id' => '1']
+    //             ]
+    //         ]
+    //     ]);
+    // }
 }
