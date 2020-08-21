5ba9cbc6a06eb0e78d8bb5bf61ddc3a6e4958851
Amirmasoud <amirmasood33@gmail.com>
2020-08-21 22:00:16 +0430

    Init feature tests

---
+++ tests/Feature/QueryTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Pepper\Tests\Feature;
+
+
+use Pepper\Tests\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+
+class QueryTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        // ...
+    }
+}
