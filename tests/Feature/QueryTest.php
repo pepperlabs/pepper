a0c857f441aa45eb36859ec7f26d71fa61a5d399
Amirmasoud <amirmasood33@gmail.com>
2020-08-25 22:26:43 +0430

    Update tests

--- tests/Feature/QueryTest.php
+++ tests/Feature/QueryTest.php
@@ -5,69 +5,11 @@ namespace Tests\Feature;
 
 use Tests\TestCaseDatabase;
 use Tests\Support\Models\Post;
-use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithoutMiddleware;
-use Illuminate\Support\Facades\DB;
 
 class QueryTest extends TestCaseDatabase
 {
-    public function testWithoutSelectFields(): void
+    public function testExample()
     {
-        $post = factory(Post::class)->create([
-            'title' => 'Title of the post',
-        ]);
-
-        $graphql = <<<GRAQPHQL
-        {
-            post_by_pk(id: $post->id) {
-            id
-            title
-            }
-        }
-        GRAQPHQL;
-
-        $response = $this->call('POST', '/graphql', [
-            'query' => $graphql,
-        ]);
-
-        $expectedResult = [
-            'data' => [
-                'post_by_pk' => [
-                    'id' => "$post->id",
-                    'title' => 'Title of the post',
-                ],
-            ],
-        ];
-
-        $this->assertEquals($response->getStatusCode(), 200);
-        $this->assertEquals($expectedResult, $response->json());
+        $this->assertTrue(true);
     }
-
-    // public function testStatus()
-    // {
-    //     $response = dd($this->getJson('http://localhost:8000/graphql'));
-
-    //     $response->assertStatus(200);
-    // }
-
-    // /**
-    //  * A basic test example.
-    //  *
-    //  * @return void
-    //  */
-    // public function testSimpleQueryByPk()
-    // {
-    //     $response = $this->withHeaders([
-    //         'Content-Type' => 'application/json',
-    //     ])->json('GET', '/graphql', [
-    //         'query' => 'query {user_by_pk(id: 1){id}}'
-    //     ]);
-    //     $response->assertStatus(200)->assertExactJson([
-    //         'data' => [
-    //             'user_by_pk' => [
-    //                 ['id' => '1']
-    //             ]
-    //         ]
-    //     ]);
-    // }
 }
