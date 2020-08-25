7b07a4abe8acc02619daa6ce4bd3a7436c595dbf
Amirmasoud <amirmasood33@gmail.com>
2020-08-26 01:14:04 +0430

    First actual test

--- tests/Feature/QueryTest.php
+++ tests/Feature/QueryTest.php
@@ -3,11 +3,40 @@
 namespace Tests\Feature;
 
 use Tests\TestCaseDatabase;
+use Tests\Support\Models\Post;
 
 class QueryTest extends TestCaseDatabase
 {
-    public function testExample()
+    /** @test */
+    public function simple_query_by_pk()
     {
-        $this->assertTrue(true);
+        $post = factory(Post::class)->create([
+            'title' => 'Title of the post',
+        ]);
+
+        $graphql = <<<GRAQPHQL
+{
+    post_by_pk(id: $post->id) {
+        id
+        title
+    }
+}
+GRAQPHQL;
+
+        $response = $this->call('GET', '/graphql', [
+            'query' => $graphql,
+        ]);
+
+        $expectedResult = [
+            'data' => [
+                'post_by_pk' => [
+                    'id' => "$post->id",
+                    'title' => 'Title of the post',
+                ],
+            ],
+        ];
+
+        $this->assertEquals($response->getStatusCode(), 200);
+        $this->assertEquals($expectedResult, $response->json());
     }
 }
