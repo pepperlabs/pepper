3f37434019270ae7a7d8b758ef8f20930646d5f0
Amirmasoud <amirmasood33@gmail.com>
2020-08-26 15:53:23 +0430

    Simple query tests

--- tests/Feature/QueryTest.php
+++ tests/Feature/QueryTest.php
@@ -15,13 +15,13 @@ class QueryTest extends TestCaseDatabase
         ]);
 
         $graphql = <<<GRAQPHQL
-{
-    post_by_pk(id: $post->id) {
-        id
-        title
-    }
-}
-GRAQPHQL;
+        {
+            post_by_pk(id: $post->id) {
+                id
+                title
+            }
+        }
+        GRAQPHQL;
 
         $response = $this->call('GET', '/graphql', [
             'query' => $graphql,
@@ -36,8 +36,181 @@ GRAQPHQL;
             ],
         ];
 
-        $response->assertSuccessful();
+        $response->assertOk();
+        $this->assertEquals($expectedResult, $response->json());
+    }
 
+    /** @test */
+    public function simple_query_by_pk_not_found()
+    {
+        $graphql = <<<GRAQPHQL
+        {
+            post_by_pk(id: 0) {
+                id
+                title
+            }
+        }
+        GRAQPHQL;
+
+        $response = $this->call('GET', '/graphql', [
+            'query' => $graphql,
+        ]);
+
+        $expectedResult = [
+            'data' => [
+                'post_by_pk' => null
+            ],
+        ];
+
+        $response->assertOk();
+        $this->assertEquals($expectedResult, $response->json());
+    }
+
+    /** @test */
+    public function simple_query()
+    {
+        $post_1 = factory(Post::class)->create(
+            ['title' => 'Title of the post #1'],
+        );
+
+        $post_2 = factory(Post::class)->create(
+            ['title' => 'Title of the post #2'],
+        );
+
+        $post_3 = factory(Post::class)->create(
+            ['title' => 'Title of the post #3'],
+        );
+
+        $graphql = <<<GRAQPHQL
+            {
+                post {
+                    id
+                    title
+                }
+            }
+            GRAQPHQL;
+
+        $response = $this->call('GET', '/graphql', [
+            'query' => $graphql,
+        ]);
+
+        $expectedResult = [
+            'data' => [
+                'post' => [
+                    [
+                        'id' => "$post_1->id",
+                        'title' => 'Title of the post #1',
+                    ],
+                    [
+                        'id' => "$post_2->id",
+                        'title' => 'Title of the post #2',
+                    ],
+                    [
+                        'id' => "$post_3->id",
+                        'title' => 'Title of the post #3',
+                    ],
+                ],
+            ],
+        ];
+
+        $response->assertOk();
+        $this->assertEquals($expectedResult, $response->json());
+    }
+
+    /** @test */
+    public function simple_query_not_found()
+    {
+        $graphql = <<<GRAQPHQL
+                {
+                    post {
+                        id
+                        title
+                    }
+                }
+                GRAQPHQL;
+
+        $response = $this->call('GET', '/graphql', [
+            'query' => $graphql,
+        ]);
+
+        $expectedResult = [
+            'data' => [
+                'post' => []
+            ],
+        ];
+
+        $response->assertOk();
+        $this->assertEquals($expectedResult, $response->json());
+    }
+
+    /** @test */
+    public function simple_query_aggregate()
+    {
+        $post_1 = factory(Post::class)->create(
+            ['title' => 'Title of the post #1'],
+        );
+
+        $post_2 = factory(Post::class)->create(
+            ['title' => 'Title of the post #2'],
+        );
+
+        $post_3 = factory(Post::class)->create(
+            ['title' => 'Title of the post #3'],
+        );
+
+        $graphql = <<<GRAQPHQL
+                query {
+                    post_aggregate {
+                        aggregate {
+                            count
+                            sum {
+                                ...aggregateOnFragment
+                            }
+                            avg {
+                                ...aggregateOnFragment
+                            }
+                            max {
+                                ...aggregateOnFragment
+                            }
+                            min {
+                                ...aggregateOnFragment
+                            }
+                        }
+                    }
+                }
+
+                fragment aggregateOnFragment on PostResultAggregateType {
+                    id
+                }
+                GRAQPHQL;
+
+        $response = $this->call('GET', '/graphql', [
+            'query' => $graphql,
+        ]);
+
+        $expectedResult = [
+            'data' => [
+                'post_aggregate' => [
+                    'aggregate' => [
+                        'count' => 3,
+                        'sum' => [
+                            'id' => array_sum([$post_1->id, $post_2->id, $post_3->id]),
+                        ],
+                        'avg' => [
+                            'id' => array_sum([$post_1->id, $post_2->id, $post_3->id]) / 3,
+                        ],
+                        'max' => [
+                            'id' => max([$post_1->id, $post_2->id, $post_3->id]),
+                        ],
+                        'min' => [
+                            'id' => min([$post_1->id, $post_2->id, $post_3->id]),
+                        ],
+                    ]
+                ],
+            ],
+        ];
+
+        $response->assertOk();
         $this->assertEquals($expectedResult, $response->json());
     }
 }
