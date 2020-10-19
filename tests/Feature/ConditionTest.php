2140fd79b1eac01b5b1b92f5485a9ae9f5c29c70
amirmasoud <amirmasood33@gmail.com>
2020-10-19 20:28:40 +0330

    50% condition test coverage

---
+++ tests/Feature/ConditionTest.php
@@ -0,0 +1,339 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\Support\Models\Post;
+use Tests\TestCaseDatabase;
+
+class ConditionTest extends TestCaseDatabase
+{
+    /** @test */
+    public function equal_simple_query()
+    {
+        $post = factory(Post::class)->create([
+            'title' => 'TITLE',
+        ]);
+
+        $graphql = '{
+            post (where: {title: {_eq: "TITLE"}}) {
+                id
+                title
+            }
+        }';
+
+        $response = $this->call('GET', '/graphql', [
+            'query' => $graphql,
+        ]);
+
+        $expectedResult = [
+            'data' => [
+                'post' => [
+                    [
+                        'id' => "$post->id",
+                        'title' => 'TITLE',
+                    ],
+                ],
+            ],
+        ];
+
+        $response->assertOk();
+        $this->assertEquals($expectedResult, $response->json());
+    }
+
+    /** @test */
+    public function not_equal_simple_query()
+    {
+        $post = factory(Post::class)->create([
+            'title' => 'TITLE',
+        ]);
+
+        $graphql = '{
+            post (where: {title: {_neq: "SUBJECT"}}) {
+                id
+                title
+            }
+        }';
+
+        $response = $this->call('GET', '/graphql', [
+            'query' => $graphql,
+        ]);
+
+        $expectedResult = [
+            'data' => [
+                'post' => [
+                    [
+                        'id' => "$post->id",
+                        'title' => 'TITLE',
+                    ],
+                ],
+            ],
+        ];
+
+        $response->assertOk();
+        $this->assertEquals($expectedResult, $response->json());
+    }
+
+    /** @test */
+    public function greater_than_simple_query()
+    {
+        $post = factory(Post::class)->create([
+            'title' => 'TITLE',
+        ]);
+
+        $graphql = '{
+            post (where: {id: {_gt: 0}}) {
+                id
+                title
+            }
+        }';
+
+        $response = $this->call('GET', '/graphql', [
+            'query' => $graphql,
+        ]);
+
+        $expectedResult = [
+            'data' => [
+                'post' => [
+                    [
+                        'id' => "$post->id",
+                        'title' => 'TITLE',
+                    ],
+                ],
+            ],
+        ];
+
+        $response->assertOk();
+        $this->assertEquals($expectedResult, $response->json());
+    }
+
+    /** @test */
+    public function less_than_simple_query()
+    {
+        $post = factory(Post::class)->create([
+            'title' => 'TITLE',
+        ]);
+
+        $graphql = '{
+            post (where: {id: {_lt: 2}}) {
+                id
+                title
+            }
+        }';
+
+        $response = $this->call('GET', '/graphql', [
+            'query' => $graphql,
+        ]);
+
+        $expectedResult = [
+            'data' => [
+                'post' => [
+                    [
+                        'id' => "$post->id",
+                        'title' => 'TITLE',
+                    ],
+                ],
+            ],
+        ];
+
+        $response->assertOk();
+        $this->assertEquals($expectedResult, $response->json());
+    }
+
+    /** @test */
+    public function greater_than_equal_simple_query()
+    {
+        $post = factory(Post::class)->create([
+            'title' => 'TITLE',
+        ]);
+
+        $graphql = '{
+            post (where: {id: {_gte: 1}}) {
+                id
+                title
+            }
+        }';
+
+        $response = $this->call('GET', '/graphql', [
+            'query' => $graphql,
+        ]);
+
+        $expectedResult = [
+            'data' => [
+                'post' => [
+                    [
+                        'id' => "$post->id",
+                        'title' => 'TITLE',
+                    ],
+                ],
+            ],
+        ];
+
+        $response->assertOk();
+        $this->assertEquals($expectedResult, $response->json());
+    }
+
+    /** @test */
+    public function less_than_equal_simple_query()
+    {
+        $post = factory(Post::class)->create([
+            'title' => 'TITLE',
+        ]);
+
+        $graphql = '{
+                post (where: {id: {_lte: 1}}) {
+                    id
+                    title
+                }
+            }';
+
+        $response = $this->call('GET', '/graphql', [
+            'query' => $graphql,
+        ]);
+
+        $expectedResult = [
+            'data' => [
+                'post' => [
+                    [
+                        'id' => "$post->id",
+                        'title' => 'TITLE',
+                    ],
+                ],
+            ],
+        ];
+
+        $response->assertOk();
+        $this->assertEquals($expectedResult, $response->json());
+    }
+
+    /** @test */
+    public function where_in_simple_query()
+    {
+        $post = factory(Post::class)->create([
+            'title' => 'TITLE',
+        ]);
+
+        $graphql = '{
+                post (where: {id: {_in: [1]}}) {
+                    id
+                    title
+                }
+            }';
+
+        $response = $this->call('GET', '/graphql', [
+            'query' => $graphql,
+        ]);
+
+        $expectedResult = [
+            'data' => [
+                'post' => [
+                    [
+                        'id' => "$post->id",
+                        'title' => 'TITLE',
+                    ],
+                ],
+            ],
+        ];
+
+        $response->assertOk();
+        $this->assertEquals($expectedResult, $response->json());
+    }
+
+    /** @test */
+    public function where_not_in_simple_query()
+    {
+        $post = factory(Post::class)->create([
+            'title' => 'TITLE',
+        ]);
+
+        $graphql = '{
+                post (where: {id: {_nin: [0]}}) {
+                    id
+                    title
+                }
+            }';
+
+        $response = $this->call('GET', '/graphql', [
+            'query' => $graphql,
+        ]);
+
+        $expectedResult = [
+            'data' => [
+                'post' => [
+                    [
+                        'id' => "$post->id",
+                        'title' => 'TITLE',
+                    ],
+                ],
+            ],
+        ];
+
+        $response->assertOk();
+        $this->assertEquals($expectedResult, $response->json());
+    }
+
+    /** @test */
+    public function like_simple_query()
+    {
+        $post = factory(Post::class)->create([
+            'title' => 'TITLE',
+        ]);
+
+        $graphql = '{
+                post (where: {title: {_like: "TITL%"}}) {
+                    id
+                    title
+                }
+            }';
+
+        $response = $this->call('GET', '/graphql', [
+            'query' => $graphql,
+        ]);
+
+        $expectedResult = [
+            'data' => [
+                'post' => [
+                    [
+                        'id' => "$post->id",
+                        'title' => 'TITLE',
+                    ],
+                ],
+            ],
+        ];
+
+        $response->assertOk();
+        $this->assertEquals($expectedResult, $response->json());
+    }
+
+    /** @test */
+    public function not_like_simple_query()
+    {
+        $post = factory(Post::class)->create([
+            'title' => 'TITLE',
+        ]);
+
+        $graphql = '{
+                post (where: {title: {_nlike: "SUB%"}}) {
+                    id
+                    title
+                }
+            }';
+
+        $response = $this->call('GET', '/graphql', [
+            'query' => $graphql,
+        ]);
+
+        $expectedResult = [
+            'data' => [
+                'post' => [
+                    [
+                        'id' => "$post->id",
+                        'title' => 'TITLE',
+                    ],
+                ],
+            ],
+        ];
+
+        $response->assertOk();
+        $this->assertEquals($expectedResult, $response->json());
+    }
+}
